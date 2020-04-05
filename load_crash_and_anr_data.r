44a1bb06d1a56724af24aadd801eff0de56d3a6e
julianharty <julianharty@gmail.com>
2020-04-05 09:38:18 +0100

    First working script to load Pocket Code crashes and ANRs.

---
+++ load_crash_and_anr_data.r
@@ -0,0 +1,11 @@
+directory = "~/Dropbox/Google Play Console Reports/reports/catrobat/pocketcode/crashes/"
+filenames = list.files(directory, pattern = "^crashes_org\\.catrobat\\.catroid_.*overview.csv$")
+full_filenames <- paste(directory, filenames, sep="")
+head(directory)
+head(filenames)
+head(full_filenames)
+crashes_and_anrs <- lapply(full_filenames, read.csv, fileEncoding="UTF-16")
+head(crashes_and_anrs)
+crashes_and_anr_data <- do.call(rbind, crashes_and_anrs)
+head(crashes_and_anr_data)
+tail(crashes_and_anr_data)
